44fb46cdf71cc39206dcd3d35a25ec4fc6d7e73b
pizzamagician <60462699+pizzamagician@users.noreply.github.com>
2022-06-01 18:34:49 +0300

    Add files via upload

---
+++ lua/autorun/dota_invoker.lua
@@ -0,0 +1,3 @@
+player_manager.AddValidModel( "Invoker", "models/player/dota_invoker.mdl" );
+list.Set( "PlayerOptionsModel",  "Invoker", "models/player/dota_invoker.mdl" );
+sound.AddSoundOverrides("scripts/vehicles/npc_sounds_invoker.txt")
